2681209701fbe204795ff78f77543a2a713c1036
Michelle-currier <mushel@gmail.com>
2024-10-14 14:33:43 -0500

    testing the INP issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
           projects in the greater New Orleans area.
         </p>
         <TransitionButton href="/contact">
-          <button className="mt-8 bg-white text-blue-600 font-semibold py-2 px-4 rounded-full inline-flex items-center">
+          <button className="mt-8 bg-white text-orange-600 font-semibold py-2 px-4 rounded-full inline-flex items-center">
             Get in Touch
             <ArrowRightIcon className="w-5 h-5 ml-2" />
           </button>
